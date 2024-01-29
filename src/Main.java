0c8c106635217f9152448140c5858ab07e292062
Ratmir Kireev <kireevratmir@gmail.com>
2024-01-29 12:08:18 +0500

    nitial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 12_200;
+        int milya = 20;
+
+        int bonus = amount / milya;
+
+        System.out.println("Начислится бонусных миль: " + bonus);
+
+    }
+}
